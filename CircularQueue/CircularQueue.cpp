4b53c751741bfae1ec0d31318f9f27da318c6b96
vararais <varadigmaisnann@gmail.com>
2024-05-30 17:40:57 +0700

    Membuat If Else

--- CircularQueue/CircularQueue.cpp
+++ CircularQueue/CircularQueue.cpp
@@ -17,6 +17,24 @@ public:
         cin >> num;
         cout << endl;
     }
+
+    //Cek apakah antrian penuh
+    if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
+        cout << "\nQueue Overflow\n";
+    }
+    //Cek apakah antrian kosong
+    if (FRONT == -1) {
+        FRONT == 0;
+        REAR = 0;
+    }
+    else {
+        //Jika REAR berada di posisi terakhir array, kembali ke awal array
+        if (REAR == max - 1)
+            REAR = 0;
+        else
+            REAR = REAR + 1;
+    }
+    queue_array[REAR] = num;
 };
 
 int main()
